b72f5b41a6d89072564052ab31c768652194e724
gladyslau19 <37594412+gladyslau19@users.noreply.github.com>
2019-06-12 17:03:51 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -135,13 +135,13 @@ namespace bitboard
     export function sendfunckey(funckey: BBFuncKeys){
         switch (funckey)
         {
-		case BBFuncKeys.Enter: bitboard::send_function_keys(0x04);
-		case BBFuncKeys.Space: bitboard::send_function_keys(0x2c);
-		case BBFuncKeys.Backspace: bitboard::send_function_keys(0x2a);
-            case BBFuncKeys.Tab: bitboard::send_function_keys(0x2b);
-            case BBFuncKeys.Escape: bitboard::send_function_keys(0x29);
-            case BBFuncKeys.CapsLock: bitboard::send_function_keys(0x39);
-	    default: NULL;
+		case BBFuncKeys.Enter: bitboard.send_function_keys(0x04);
+		case BBFuncKeys.Space: bitboard.send_function_keys(0x2c);
+		case BBFuncKeys.Backspace: bitboard.send_function_keys(0x2a);
+            	case BBFuncKeys.Tab: bitboard.send_function_keys(0x2b);
+            	case BBFuncKeys.Escape: bitboard.send_function_keys(0x29);
+            	case BBFuncKeys.CapsLock: bitboard.send_function_keys(0x39);
+	    	default: NULL;
         }
     }
 
